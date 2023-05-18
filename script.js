d1a6f63c0014f162479ece04cd00c877b04edb05
AnythingRed <130374881+AnythingRed@users.noreply.github.com>
2023-05-18 21:46:09 +0200

    Add ability to change size of the grid

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const gridEl = document.querySelector('.grid');
+let allSquares;
+let inputNum;
 
 function generateGrid(num) {
-   for (let index = 0; index < num; index++) {
+   for (let index = 0; index < (num * num); index++) {
       const squareEl = document.createElement('div');
-      squareEl.classList.add('square')
+      squareEl.classList.add('square');
+      squareEl.style.width = 800/num + 'px';
+      squareEl.style.height = 800/num + 'px';
       gridEl.appendChild(squareEl);
    }
+   allSquares = document.querySelectorAll('.square');
+   draw();
 }
-//16x16
-generateGrid(256);
 
-let allSquares = document.getElementsByClassName('square');
+generateGrid(16);
 
-for (i = 0; i < allSquares.length; i++) {
-   allSquares[i].addEventListener('mouseover', (e) => {
-      e.target.style.backgroundColor = 'black'
+function draw() {
+   allSquares.forEach(square => {
+      square.addEventListener('mouseover', (e) => {
+         e.target.style.backgroundColor = 'black';
+      });
    });
+}
+
+function changeSize() {
+   inputNum = window.prompt('How many squares do you want per side? Please enter a number (max 100)');
+   if (!inputNum) return;
+   allSquares.forEach(square => {
+      square.remove();
+   });
+   generateGrid(+inputNum);
+   draw();
 }
